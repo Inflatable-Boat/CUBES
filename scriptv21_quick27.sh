c613f0ed7b887bd7aab527aad697d445f6bef7a4
Inflatable-Boat <rudenimour@hotmail.com>
2019-04-03 18:06:08 +0200

    scripts+little tex

--- scriptv21_quick27.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-time -o times/v21_melt_12_p079_27 -f "%E\t%U\t%S,\t%P\n" ./v21_sim.exe c 12 0.60 750000 1000  7.9 1.26610367 > logs/v21_melt_12_p079 &
-time -o times/v21_compr_12_p079_27 -f "%E\t%U\t%S,\t%P\n" ./v21_sim.exe c 12 0.40 750000 1000  7.9 1.26610367 > logs/v21_compr_12_p079 &
--- temp.1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-./v22_coords_to_order.exe datafolder/v21_20p7.60a1.27 1000 1000 t s &
-./v22_coords_to_order.exe datafolder/v21_20p7.65a1.27 1000 1000 t s &
-./v22_coords_to_order.exe datafolder/v21_20p7.70a1.27 1000 1000 t s &
-./v22_coords_to_order.exe datafolder/v21_20p7.75a1.27 1000 1000 t s &
-./v22_coords_to_order.exe datafolder/v21_20p7.80a1.27 1000 1000 t s &
-./v22_coords_to_order.exe datafolder/v21_20p7.85a1.27 1000 1000 t s &
-./v22_coords_to_order.exe datafolder/v21_20p7.90a1.27 1000 1000 t s &
-./v22_coords_to_order.exe datafolder/v21_20p7.95a1.27 1000 1000 t s &
