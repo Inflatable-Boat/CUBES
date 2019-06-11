9c3ed6a0ac3f720c80643224acf50fcc6e764703
Inflatable-Boat <rudenimour@hotmail.com>
2019-06-11 14:23:58 +0200

    tex

---
+++ scriptv35_12extra.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#/usr/bin/time -o times/v35_12pf0.40p03.0a1.57 ./v35_sim.exe c 12 0.4 300000 1000 03.0 1.57079632679489 > logs/v35_12pf0.40p03.0a1.57
+#/usr/bin/time -o times/v35_12pf0.40p04.0a1.57 ./v35_sim.exe c 12 0.4 300000 1000 04.0 1.57079632679489 > logs/v35_12pf0.40p04.0a1.57
+#/usr/bin/time -o times/v35_12pf0.40p05.0a1.57 ./v35_sim.exe c 12 0.4 300000 1000 05.0 1.57079632679489 > logs/v35_12pf0.40p05.0a1.57
+#/usr/bin/time -o times/v35_12pf0.40p06.0a1.57 ./v35_sim.exe c 12 0.4 300000 1000 06.0 1.57079632679489 > logs/v35_12pf0.40p06.0a1.57
+#/usr/bin/time -o times/v35_12pf0.40p07.0a1.57 ./v35_sim.exe c 12 0.4 300000 1000 07.0 1.57079632679489 > logs/v35_12pf0.40p07.0a1.57
+#/usr/bin/time -o times/v35_12pf0.40p08.0a1.57 ./v35_sim.exe c 12 0.4 300000 1000 08.0 1.57079632679489 > logs/v35_12pf0.40p08.0a1.57
+#/usr/bin/time -o times/v35_12pf0.40p09.0a1.57 ./v35_sim.exe c 12 0.4 300000 1000 09.0 1.57079632679489 > logs/v35_12pf0.40p09.0a1.57
+#/usr/bin/time -o times/v35_12pf0.40p10.0a1.57 ./v35_sim.exe c 12 0.4 300000 1000 10.0 1.57079632679489 > logs/v35_12pf0.40p10.0a1.57
+#/usr/bin/time -o times/v35_12pf0.40p03.0a1.47 ./v35_sim.exe c 12 0.4 300000 1000 03.0 1.47062890563334 > logs/v35_12pf0.40p03.0a1.47
+#/usr/bin/time -o times/v35_12pf0.40p04.0a1.47 ./v35_sim.exe c 12 0.4 300000 1000 04.0 1.47062890563334 > logs/v35_12pf0.40p04.0a1.47
+#/usr/bin/time -o times/v35_12pf0.40p05.0a1.47 ./v35_sim.exe c 12 0.4 300000 1000 05.0 1.47062890563334 > logs/v35_12pf0.40p05.0a1.47
+#/usr/bin/time -o times/v35_12pf0.40p06.0a1.47 ./v35_sim.exe c 12 0.4 300000 1000 06.0 1.47062890563334 > logs/v35_12pf0.40p06.0a1.47
+#/usr/bin/time -o times/v35_12pf0.40p07.0a1.47 ./v35_sim.exe c 12 0.4 300000 1000 07.0 1.47062890563334 > logs/v35_12pf0.40p07.0a1.47
+#/usr/bin/time -o times/v35_12pf0.40p08.0a1.47 ./v35_sim.exe c 12 0.4 300000 1000 08.0 1.47062890563334 > logs/v35_12pf0.40p08.0a1.47
+#/usr/bin/time -o times/v35_12pf0.40p09.0a1.47 ./v35_sim.exe c 12 0.4 300000 1000 09.0 1.47062890563334 > logs/v35_12pf0.40p09.0a1.47
+#/usr/bin/time -o times/v35_12pf0.40p10.0a1.47 ./v35_sim.exe c 12 0.4 300000 1000 10.0 1.47062890563334 > logs/v35_12pf0.40p10.0a1.47
+#/usr/bin/time -o times/v35_12pf0.40p03.0a1.37 ./v35_sim.exe c 12 0.4 300000 1000 03.0 1.36943840600457 > logs/v35_12pf0.40p03.0a1.37
+#/usr/bin/time -o times/v35_12pf0.40p04.0a1.37 ./v35_sim.exe c 12 0.4 300000 1000 04.0 1.36943840600457 > logs/v35_12pf0.40p04.0a1.37
+#/usr/bin/time -o times/v35_12pf0.40p05.0a1.37 ./v35_sim.exe c 12 0.4 300000 1000 05.0 1.36943840600457 > logs/v35_12pf0.40p05.0a1.37
+#/usr/bin/time -o times/v35_12pf0.40p06.0a1.37 ./v35_sim.exe c 12 0.4 300000 1000 06.0 1.36943840600457 > logs/v35_12pf0.40p06.0a1.37
+#/usr/bin/time -o times/v35_12pf0.40p07.0a1.37 ./v35_sim.exe c 12 0.4 300000 1000 07.0 1.36943840600457 > logs/v35_12pf0.40p07.0a1.37
+#/usr/bin/time -o times/v35_12pf0.40p08.0a1.37 ./v35_sim.exe c 12 0.4 300000 1000 08.0 1.36943840600457 > logs/v35_12pf0.40p08.0a1.37
+#/usr/bin/time -o times/v35_12pf0.40p09.0a1.37 ./v35_sim.exe c 12 0.4 300000 1000 09.0 1.36943840600457 > logs/v35_12pf0.40p09.0a1.37
+#/usr/bin/time -o times/v35_12pf0.40p10.0a1.37 ./v35_sim.exe c 12 0.4 300000 1000 10.0 1.36943840600457 > logs/v35_12pf0.40p10.0a1.37
+#/usr/bin/time -o times/v35_12pf0.40p03.0a1.27 ./v35_sim.exe c 12 0.4 300000 1000 03.0 1.26610367277950 > logs/v35_12pf0.40p03.0a1.27
+#/usr/bin/time -o times/v35_12pf0.40p04.0a1.27 ./v35_sim.exe c 12 0.4 300000 1000 04.0 1.26610367277950 > logs/v35_12pf0.40p04.0a1.27
+#/usr/bin/time -o times/v35_12pf0.40p05.0a1.27 ./v35_sim.exe c 12 0.4 300000 1000 05.0 1.26610367277950 > logs/v35_12pf0.40p05.0a1.27
+#/usr/bin/time -o times/v35_12pf0.40p06.0a1.27 ./v35_sim.exe c 12 0.4 300000 1000 06.0 1.26610367277950 > logs/v35_12pf0.40p06.0a1.27
+#/usr/bin/time -o times/v35_12pf0.40p07.0a1.27 ./v35_sim.exe c 12 0.4 300000 1000 07.0 1.26610367277950 > logs/v35_12pf0.40p07.0a1.27
+#/usr/bin/time -o times/v35_12pf0.40p08.0a1.27 ./v35_sim.exe c 12 0.4 300000 1000 08.0 1.26610367277950 > logs/v35_12pf0.40p08.0a1.27
+#/usr/bin/time -o times/v35_12pf0.40p09.0a1.27 ./v35_sim.exe c 12 0.4 300000 1000 09.0 1.26610367277950 > logs/v35_12pf0.40p09.0a1.27
+#/usr/bin/time -o times/v35_12pf0.40p10.0a1.27 ./v35_sim.exe c 12 0.4 300000 1000 10.0 1.26610367277950 > logs/v35_12pf0.40p10.0a1.27
+#/usr/bin/time -o times/v35_12pf0.40p04.0a1.16 ./v35_sim.exe c 12 0.4 300000 1000 04.0 1.15927948072741 > logs/v35_12pf0.40p04.0a1.16
+#/usr/bin/time -o times/v35_12pf0.40p05.0a1.16 ./v35_sim.exe c 12 0.4 300000 1000 05.0 1.15927948072741 > logs/v35_12pf0.40p05.0a1.16
+#/usr/bin/time -o times/v35_12pf0.40p06.0a1.16 ./v35_sim.exe c 12 0.4 300000 1000 06.0 1.15927948072741 > logs/v35_12pf0.40p06.0a1.16
+#/usr/bin/time -o times/v35_12pf0.40p07.0a1.16 ./v35_sim.exe c 12 0.4 300000 1000 07.0 1.15927948072741 > logs/v35_12pf0.40p07.0a1.16
+#/usr/bin/time -o times/v35_12pf0.40p08.0a1.16 ./v35_sim.exe c 12 0.4 300000 1000 08.0 1.15927948072741 > logs/v35_12pf0.40p08.0a1.16
+#/usr/bin/time -o times/v35_12pf0.40p09.0a1.16 ./v35_sim.exe c 12 0.4 300000 1000 09.0 1.15927948072741 > logs/v35_12pf0.40p09.0a1.16
+#/usr/bin/time -o times/v35_12pf0.40p10.0a1.16 ./v35_sim.exe c 12 0.4 300000 1000 10.0 1.15927948072741 > logs/v35_12pf0.40p10.0a1.16
+#/usr/bin/time -o times/v35_12pf0.40p11.0a1.16 ./v35_sim.exe c 12 0.4 300000 1000 11.0 1.15927948072741 > logs/v35_12pf0.40p11.0a1.16
+
+# /usr/bin/time -o times/v35_12pf0.40p11.0a1.27 ./v35_sim.exe c 12 0.4 300000 1000 11.0 1.26610367277950 > logs/v35_12pf0.40p11.0a1.27 &
+# /usr/bin/time -o times/v35_12pf0.40p12.0a1.27 ./v35_sim.exe c 12 0.4 300000 1000 12.0 1.26610367277950 > logs/v35_12pf0.40p12.0a1.27 &
+# /usr/bin/time -o times/v35_12pf0.40p12.0a1.16 ./v35_sim.exe c 12 0.4 300000 1000 12.0 1.15927948072741 > logs/v35_12pf0.40p12.0a1.16 &
+# /usr/bin/time -o times/v35_12pf0.40p13.0a1.16 ./v35_sim.exe c 12 0.4 300000 1000 13.0 1.15927948072741 > logs/v35_12pf0.40p13.0a1.16 &
+# /usr/bin/time -o times/v35_12pf0.40p14.0a1.16 ./v35_sim.exe c 12 0.4 300000 1000 14.0 1.15927948072741 > logs/v35_12pf0.40p14.0a1.16 &
+# /usr/bin/time -o times/v35_12pf0.40p15.0a1.16 ./v35_sim.exe c 12 0.4 300000 1000 15.0 1.15927948072741 > logs/v35_12pf0.40p15.0a1.16 &
+
+/usr/bin/time -o times/v35_12pf0.40p13.0a1.27 ./v35_sim.exe c 12 0.4 300000 1000 13.0 1.26610367277950 > logs/v35_12pf0.40p13.0a1.27 &
+/usr/bin/time -o times/v35_12pf0.40p14.0a1.27 ./v35_sim.exe c 12 0.4 300000 1000 14.0 1.26610367277950 > logs/v35_12pf0.40p14.0a1.27 &
+/usr/bin/time -o times/v35_12pf0.40p16.0a1.16 ./v35_sim.exe c 12 0.4 300000 1000 16.0 1.15927948072741 > logs/v35_12pf0.40p16.0a1.16 &
+/usr/bin/time -o times/v35_12pf0.40p17.0a1.16 ./v35_sim.exe c 12 0.4 300000 1000 17.0 1.15927948072741 > logs/v35_12pf0.40p17.0a1.16 &
